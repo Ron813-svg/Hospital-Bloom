735f9484742efe4f667eb0ba4566d2f8d232fc49
RobbieLarios2024 <robbieavellana@gmail.com>
2024-07-31 22:49:21 -0600

    Error no tenemos uuid

--- app/src/main/java/RecyclerViewHelpers/Adaptador.kt
+++ app/src/main/java/RecyclerViewHelpers/Adaptador.kt
@@ -5,6 +5,7 @@ import Modelos.tbPacientes
 import android.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.recyclerview.widget.RecyclerView
 import com.robbielarios.ronyramirez.hospitalbloom.R
 import kotlinx.coroutines.Dispatchers
@@ -47,6 +48,33 @@ class Adaptador(private var Data: List<tbPacientes>): RecyclerView.Adapter<ViewH
             dialog.show()
 
         }
+
+        holder.imgEditar.setOnClickListener {
+            //Creo mi Alerta para editar
+            val context = holder.itemView.context
+
+            val builder = AlertDialog.Builder(context)
+            builder.setTitle("Actualizar")
+
+            //Agregarle un cuadro de texto
+            //donde el usuario va a escribir el nuevo nombre
+            val cuadroTexto = EditText(context)
+            cuadroTexto.setHint(item.nombrePaciente)
+            builder.setView(cuadroTexto)
+
+            //Botones
+            builder.setPositiveButton("Actualizar"){
+                    dialog, wich ->
+                actualizarDato(cuadroTexto.text.toString(), item.nombrePaciente)
+            }
+            builder.setNegativeButton("Cancelar"){
+                    dialog, wich ->
+                dialog.dismiss()
+            }
+            val dialog = builder.create()
+            dialog.show()
+
+        }
     }
 
     fun setData(newData: List<tbPacientes>) {
@@ -55,6 +83,25 @@ class Adaptador(private var Data: List<tbPacientes>): RecyclerView.Adapter<ViewH
     }
 
     //aca empiezo yo xd
+
+    fun actualizarDato(nuevoNombre: String, nombrePaciente: String){
+        GlobalScope.launch(Dispatchers.IO){
+            //1- Creo un obj de la clase conexion
+            val objConexion = Conexion().cadenaConexion()
+
+            //2- Creo una variable que contenga un PrepareStatement
+            val updateCancion = objConexion?.prepareStatement("update pacientes set Nombre = ? where idpaciente = ?")!!
+            updateCancion.setString(1, nuevoNombre)
+            updateCancion.setString(2, nombrePaciente)
+            updateCancion.executeUpdate()
+
+            val commit = objConexion.prepareStatement("commit")
+            commit.executeUpdate()
+
+        }
+    }
+
+
 
     /////////// todo: Eliminar datos
     fun eliminarDatos(nombrePaciente: String, posicion: Int){
@@ -69,9 +116,9 @@ class Adaptador(private var Data: List<tbPacientes>): RecyclerView.Adapter<ViewH
 
             //2- creo una variable que contenga
             //un PrepareStatement
-            val deleteCancion = objConexion?.prepareStatement("delete pacientes where Nombre = ?")!!
-            deleteCancion.setString(1, nombrePaciente)
-            deleteCancion.executeUpdate()
+            val deletePaciente = objConexion?.prepareStatement("delete pacientes where Nombre = ?")!!
+            deletePaciente.setString(1, nombrePaciente)
+            deletePaciente.executeUpdate()
 
             val commit = objConexion.prepareStatement("commit")
             commit.executeUpdate()
